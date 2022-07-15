dbcfcb9d7de23a3dc9c6ebab25104d1e4a051449
nikopapchenko <nikopapchenko@gmail.com>
2022-07-15 23:42:39 +0200

    old

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 // _______________________________________________________________SPOILER on jQuery
 $(document).ready(function () {
 
-    $('.skils__title').click(function (event) {
-        if ($('.skils').hasClass('one')) {
-            $('.skils__title').not($(this)).removeClass('active');
-            $('.skils__text').not($(this).next()).slideUp(200);
-        }
-        $(this).toggleClass('active').next().slideToggle(600);
-    });
+  $('.skils__title').click(function (event) {
+    if ($('.skils').hasClass('one')) {
+      $('.skils__title').not($(this)).removeClass('active');
+      $('.skils__text').not($(this).next()).slideUp(200);
+    }
+    $(this).toggleClass('active').next().slideToggle(600);
+  });
+
+});
+
+$(document).ready(function () {
+
+  $('.skils__titleMobile').click(function (event) {
+    if ($('.skils__mobileVersion').hasClass('one')) {
+      $('.skils__titleMobile').not($(this)).removeClass('active');
+      $('.skils__text1').not($(this).next()).slideUp(200);
+    }
+    $(this).toggleClass('active').next().slideToggle(600);
+  });
 
 });
 // _______________________________________________________________END
@@ -16,51 +28,43 @@ $(document).ready(function () {
 
 $(document).ready(function () {
 
-    $('.header__burger').click(function (event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
 
 });
 // _______________________________________________________________END
 
 
+// _______________________________________________________________DARK_MODE on jQuery
 var darkMode;
 
-if (localStorage.getItem('dark-mode')) {  
-  // if dark mode is in storage, set variable with that value
-  darkMode = localStorage.getItem('dark-mode');  
-} else {  
-  // if dark mode is not in storage, set variable to 'light'
-  darkMode = 'light';  
+if (localStorage.getItem('dark-mode')) {
+  darkMode = localStorage.getItem('dark-mode');
+} else {
+  darkMode = 'light';
 }
 
-// set new localStorage value
 localStorage.setItem('dark-mode', darkMode);
 
-
 if (localStorage.getItem('dark-mode') == 'dark') {
-  // if the above is 'dark' then apply .dark to the body
-  $('body').addClass('dark');  
-  // hide the 'dark' button
+  $('body').addClass('dark');
   $('.dark-button').hide();
-  // show the 'light' button
   $('.light-button').show();
 }
 
-$('.dark-button').on('click', function() {  
-    $('.dark-button').hide();
-    $('.light-button').show();
-    $('body').addClass('dark');  
-    // set stored value to 'dark'
-    localStorage.setItem('dark-mode', 'dark');
-  });
-  
-  
-  $('.light-button').on('click', function() {  
-    $('.light-button').hide();
-    $('.dark-button').show();
-    $('body').removeClass('dark');
-    // set stored value to 'light'
-    localStorage.setItem('dark-mode', 'light');   
-  });
+$('.dark-button').on('click', function () {
+  $('.dark-button').hide();
+  $('.light-button').show();
+  $('body').addClass('dark');
+  localStorage.setItem('dark-mode', 'dark');
+});
+
+$('.light-button').on('click', function () {
+  $('.light-button').hide();
+  $('.dark-button').show();
+  $('body').removeClass('dark');
+  localStorage.setItem('dark-mode', 'light');
+});
+  // _______________________________________________________________END
